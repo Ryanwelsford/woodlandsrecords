4f23d46cd876a8149fca2bd4a374a674352b8cce
Amaan <amaanhassim@gmail.com>
2020-06-15 18:28:58 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -8,34 +8,10 @@ require '../functions.php';
 
 if(isset($_POST['submit']))
 {
-
-    
-
-    // $stmt = $pdo->prepare('INSERT INTO students (studentid,firstname,middlename,surname,studentstatus,dormancyreason,termaddress,nonaddress,phonenum,email,coursecode,entryqual)
-    //                         VALUES (:studentid, :firstname, :middlename, :surname, :studentstatus, :dormancyreason, :termaddress, :nonaddress, :phonenum, :email, :coursecode, :entryqual)');
-    // $values = [
-    //     'studentid' => $_POST['studentid'],
-    //     'firstname' => $_POST['firstname'],
-    //     'middlename' => $_POST['middlename'],
-    //     'surname' => $_POST['surname'],
-    //     'studentstatus' => $_POST['studentstatus'],
-    //     'dormancyreason' => $_POST['dormancy'],
-    //     'termaddress' => $_POST['termaddress'],
-    //     'nonaddress' => $_POST['nontermaddress'],
-    //     'phonenum' => $_POST['number'],
-    //     'email' => $_POST['email'],
-    //     'coursecode' => $_POST['coursecode'],
-    //     'entryqual' => $_POST['entryqual']
-    // ];
+    //when submit button is pressed submit the information to the students table
     save($pdo,'students', $_POST['student'], 'id');
-    // $stmt->execute($values);
     
-    //$this->studenttable->insert($values);
 }
-
-// ob_start();
-// require '../templates/index.html.php';
-// $content = ob_get_clean();
 $content = loadtemplate('../templates/index.html.php',[]);
 $header = 'Create Student Record';
 $title = 'Create Student Record';
