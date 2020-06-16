b8050b2a544c0baad985d460a38b63780e70df20
Amaan <amaanhassim@gmail.com>
2020-06-16 15:17:03 +0100

    commit

--- public/liststaff.php
+++ public/liststaff.php
@@ -1,17 +1,19 @@
 <?php
 require '../database.php';
 require '../loadtemplate.php';
+require '../functions.php';
 
 if(isset($_GET['submit']))
 {
-    $search = $pdo->prepare('SELECT * FROM staff WHERE staffid= :staffid');
+    // $search = $pdo->prepare('SELECT * FROM staff WHERE staffid= :staffid');
 
-    $values = [
-        'staffid' => $_GET['search']
-    ];
+    // $values = [
+    //     'staffid' => $_GET['search']
+    // ];
 
-    $search->execute($values);
-    $stmt = $search->fetchAll();
+    // $search->execute($values);
+    // $stmt = $search->fetchAll();
+    $stmt = find($pdo,'staff','staffid',$_GET['search']);
 
     $templatevars = [
         'stmt' => $stmt,
@@ -20,8 +22,7 @@ if(isset($_GET['submit']))
     ];
 }
 else{
-$stmt = $pdo->prepare('SELECT * FROM staff');
-$stmt->execute();
+$stmt = findAll($pdo,'staff');
 
 $templatevars = [
     'stmt' => $stmt,
