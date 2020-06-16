e7bb207b322f2eeea89734d12a81c30edbbf2888
Amaan <amaanhassim@gmail.com>
2020-06-16 15:13:26 +0100

    commit

--- public/createstaff.php
+++ public/createstaff.php
@@ -1,30 +1,12 @@
 <?php
 require '../database.php';
 require '../loadtemplate.php';
+require '../functions.php';
 if(isset($_POST['submit']))
 {
-    $stmt = $pdo->prepare('INSERT INTO staff (staffstatus, dormancyreason, firstname, middlename, surname, staffid, address, phonenumber, email, roles, specialistsub)
-                            VALUES (:staffstatus, :dormancyreason, :firstname, :middlename, :surname, :staffid, :address, :phonenumber, :email, :roles, :specialistsub)');
-
-    $unsigned = $pdo->prepare('INSERT INTO unassignedstaff (staffstatus, dormancyreason, firstname, middlename, surname, staffid, address, phonenumber, email, roles, specialistsub)
-                            VALUES (:staffstatus, :dormancyreason, :firstname, :middlename, :surname, :staffid, :address, :phonenumber, :email, :roles, :specialistsub)');
-
-    $values = [
-        'staffstatus' => $_POST['staffstatus'],
-        'dormancyreason' => $_POST['dormancy'],
-        'firstname' => $_POST['firstname'],
-        'middlename' => $_POST['middlename'],
-        'surname' => $_POST['surname'],
-        'staffid' => $_POST['staffid'],
-        'address' => $_POST['address'],
-        'phonenumber' => $_POST['phonenum'],
-        'email' => $_POST['email'],
-        'roles' => $_POST['roles'],
-        'specialistsub' => $_POST['specialsub']
-    ];
-
-    $stmt->execute($values);
-    $unsigned->execute($values);
+//when submit button pressed insert info in the 2 tables
+    save($pdo,'staff',$_POST['staff'],'id');
+    save($pdo,'unassignedstaff', $_POST['staff'],'id');
 
 }
 $content = loadtemplate('../templates/createstaff.html.php',[]);
--- templates/createstaff.html.php
+++ templates/createstaff.html.php
@@ -17,12 +17,12 @@
                             <p></p>
                             <p></p>
                             <label for="stastatus">Staff Status: </label>
-                            <select id="stastatus" name="staffstatus">
+                            <select id="stastatus" name="staff[staffstatus]">
                                 <option name="live">Live</option>
                                 <option name="Dor">Dormant</option>
                             </select>
 
-                            <label for="dor">Reason for dormancy: </label> <textarea id="dor" name="dormancy"></textarea>
+                            <label for="dor">Reason for dormancy: </label> <textarea id="dor" name="staff[dormancyreason]"></textarea>
                         </div>    
                     </div>
 
@@ -30,10 +30,10 @@
                         <div class="data">
                             <p></p>
                             <p></p>
-                            <label id="fname">First name: </label> <input type="text" name="firstname" id="fname">
-                            <label id="mname">Middle name: </label> <input type="text" name="middlename" id="mname">
-                            <label id="sname">Surname: </label> <input type="text" name="surname" id="sname">
-                            <label id="id">Staff ID: </label> <input type="text" name="staffid" id="id">
+                            <label id="fname">First name: </label> <input type="text" name="staff[firstname]" id="fname">
+                            <label id="mname">Middle name: </label> <input type="text" name="staff[middlename]" id="mname">
+                            <label id="sname">Surname: </label> <input type="text" name="staff[surname]" id="sname">
+                            <label id="id">Staff ID: </label> <input type="text" name="staff[staffid]" id="id">
                         </div>
                     </div>
 
@@ -41,9 +41,9 @@
                         <div class="data">
                             <p></p>
                             <p></p>
-                            <label for="add">Address: </label> <textarea id="add" name="address"></textarea>
-                            <label for="num">Phone Number: </label> <input type="text" name="phonenum" id="num">
-                            <label for="em">Email Address: </label> <input type="text" name="email" id="em">
+                            <label for="add">Address: </label> <textarea id="add" name="staff[address]"></textarea>
+                            <label for="num">Phone Number: </label> <input type="text" name="staff[phonenumber]" id="num">
+                            <label for="em">Email Address: </label> <input type="text" name="staff[email]" id="em">
                         </div>
                     </div>
 
@@ -51,8 +51,8 @@
                         <div class="data">
                             <p></p>
                             <p></p>
-                            <label for="ro">Roles: </label> <textarea name="roles" id="ro"></textarea>
-                            <label for="spe">Specailist Subjects: </label> <textarea name="specialsub" id="spe"></textarea>
+                            <label for="ro">Roles: </label> <textarea name="staff[roles]" id="ro"></textarea>
+                            <label for="spe">Specailist Subjects: </label> <textarea name="staff[specialistsub]" id="spe"></textarea>
                         </div>
                     </div>
                 </div>
