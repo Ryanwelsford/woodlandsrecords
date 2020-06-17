8667b3c4fc1a53d86ddc285bddfaf48030959e69
Amaan <amaanhassim@gmail.com>
2020-06-17 18:46:58 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php
 require '../database.php';
-// require '../databasetable.php';
+require '../databasetable.php';
 require '../loadtemplate.php';
 require '../functions.php';
 
-// $studenttable = new databasetable($pdo,'students','studentid');
+$studenttable = new databasetable($pdo,'students','id');
 
 if(isset($_POST['submit']))
 {
     //when submit button is pressed submit the information to the students table
-    save($pdo,'students', $_POST['student'], 'id');
+    $studenttable->save($_POST['student']);
     
 }
 $content = loadtemplate('../templates/index.html.php',[]);
