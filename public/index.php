a66668b114507895164e0ee4c982f411712fc6bf
Amaan <amaanhassim@gmail.com>
2020-06-24 14:31:45 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ require '../Classes/databasetable.php';
 require '../loadtemplate.php';
 require '../autoloader/autoloader.php';
 
+//test test test
+
 $routes = new \Diary\routes();
 $entrypoint = new \RWCSY2028\EntryPoint($routes);
 $entrypoint->run();
