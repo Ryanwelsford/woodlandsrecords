c86a8aefe517135124c62a1d5889741cc81ea7ee
Ryan <ryanwelsford@hotmail.com>
2020-08-06 12:10:28 +0100

    basics for attendance updated

--- Diary/Controllers/Attendance.php
+++ Diary/Controllers/Attendance.php
@@ -3,4 +3,17 @@ namespace Diary\Controllers;
 
 class Attendance {
     
+    //so the pages we will need starting at the top
+
+
+    //a method to create a new attendance document for the week
+    //this will need to include a method to select which module to input students attendance
+
+    //the creation method should also probably check to see if an attendance doc has been submitted within the current week
+
+    //the ability to archive any attendance record
+
+    //action poor attendance in some way, i.e. for those that have poor attendance produce a printout for it ..?
+
+    //monitor should display which students have not attended some count of modules within a certain time frame?
 }
